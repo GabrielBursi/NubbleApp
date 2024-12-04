33303d31ed4cd6c45f5f7a787b8450e1e48393d4
Gabriel Bursi <gabriellbursii@gmail.com>
2024-12-03 22:13:57 -0300

    chore: variavel env test mock

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
+import Config from 'react-native-config'
 
 async function setupDevEnv() {
-	if (!__DEV__) {
+	if (Number(Config.LOAD_MOCK)) {
 		return false
 	}
 
--- src/types/react-native-config.d.ts
+++ src/types/react-native-config.d.ts
@@ -2,6 +2,7 @@ declare module 'react-native-config' {
 	export interface NativeConfig {
 		LOAD_STORYBOOK?: 'true' | 'false'
 		API_URL?: string
+		LOAD_MOCK?: '0' | '1'
 	}
 
 	export const Config: NativeConfig
