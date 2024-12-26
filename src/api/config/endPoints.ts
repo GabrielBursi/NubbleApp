101b35f2241c41c2f7c22b243083be6667c142d7
Gabriel Bursi <gabriellbursii@gmail.com>
2024-12-26 20:58:21 -0300

    refactor: enum api endpoints

--- src/api/config/endPoints.ts
+++ src/api/config/endPoints.ts
@@ -1,4 +1,4 @@
-export const END_POINTS_API = {
-	COMMENT: '/user/post_comment',
-	POST: '/user/post',
-} as const
+export enum END_POINTS_API {
+	COMMENT = '/user/post_comment',
+	POST = '/user/post',
+}
