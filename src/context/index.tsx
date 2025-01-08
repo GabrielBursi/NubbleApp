6301efe6c45b275a4f48de48033489bb5fa7e41a
Gabriel Bursi <gabriellbursii@gmail.com>
2025-01-07 21:57:25 -0300

    chore: add auth crendentials context provider on global context provider

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,9 +1,15 @@
 import { PropsWithChildren } from 'react'
 
+import { AuthCredentialsProvider } from '@/services/auth'
 import { ToastProvider } from '@/services/toast'
 
 //? Only use ToastProvider if it is using Context implementation. Zustand implementation doesn't need a provider
+//? Only use AuthCredentialsProvider if it is using Context implementation. Zustand implementation doesn't need a provider
 
 export const ContextProvider = ({ children }: Readonly<PropsWithChildren>) => {
-	return <ToastProvider>{children}</ToastProvider>
+	return (
+		<AuthCredentialsProvider>
+			<ToastProvider>{children}</ToastProvider>
+		</AuthCredentialsProvider>
+	)
 }
--- src/providers/TestProvider.tsx
+++ src/providers/TestProvider.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@shopify/restyle'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-//? Only use ToastProvider if it is using Context implementation. Zustand implementation doesn't need a provider
+//? Only use ContextProvider if it is using Context implementation. Zustand implementation doesn't need a provider
 // import { ContextProvider } from '@/context'
 import { appTheme } from '@/styles'
 
