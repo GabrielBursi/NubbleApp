2ae92c65be8a5dc744e5d2838ab394cffa356888
Gabriel Bursi <gabriellbursii@gmail.com>
2025-01-08 22:57:50 -0300

    feat: storage implementation with mmkv

---
+++ src/services/storage/config/MMKV.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { MMKV } from 'react-native-mmkv'
+
+import { Storage } from '../models'
+
+const MMKVInstance = new MMKV()
+export const MMKVStorage: Storage = {
+	getItem: (key) => {
+		const item = MMKVInstance.getString(key)
+		if (item) {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+			return JSON.parse(item)
+		}
+		return null
+	},
+	setItem: async (key, value) => MMKVInstance.set(key, JSON.stringify(value)),
+	removeItem: async (key) => MMKVInstance.delete(key),
+}
--- src/services/storage/config/index.ts
+++ src/services/storage/config/index.ts
@@ -1,1 +1,2 @@
 export * from './AsyncStorage'
+export * from './MMKV'
