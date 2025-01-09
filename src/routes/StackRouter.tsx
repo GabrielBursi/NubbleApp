3f4fce71108b940b655de8560b3fe1bb09e63e4f
Gabriel Bursi <gabriellbursii@gmail.com>
2025-01-08 21:26:28 -0300

    feat(auth): loading state in router

--- src/routes/StackRouter.tsx
+++ src/routes/StackRouter.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { ActivityIndicator } from 'react-native'
 
-import { useAuthCredentials } from '@/services/auth'
+import { Box } from '@/components'
+import { useAuthCredentials, useAuthCredentialsService } from '@/services/auth'
 
 import { AppStackRouter } from './stack/AppStack'
 import { AuthStackRouter } from './stack/AuthStack'
 
 export const StackRouter = () => {
 	const auth = useAuthCredentials()
+	const { isLoading } = useAuthCredentialsService()
+
+	if (isLoading)
+		return (
+			<Box
+				flex={1}
+				backgroundColor="background"
+				justifyContent="center"
+				alignItems="center"
+			>
+				<ActivityIndicator size="large" />
+			</Box>
+		)
 
 	return auth ? <AppStackRouter /> : <AuthStackRouter />
 }
