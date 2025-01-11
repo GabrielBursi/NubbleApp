442fa0c17bf2e4ebfa3de089e16aba19fbe527a5
Gabriel Bursi <gabriellbursii@gmail.com>
2025-01-11 20:04:36 -0300

    refactor(useAuthValueIsAvailable): jsdoc, unvailable state and query enabled

--- src/domain/Auth/useCases/useAuthValueIsAvailable/useAuthValueIsAvailable.test.tsx
+++ src/domain/Auth/useCases/useAuthValueIsAvailable/useAuthValueIsAvailable.test.tsx
@@ -1,10 +1,14 @@
 import { renderHook, waitFor } from '@testing-library/react-native'
+import { http, HttpResponse } from 'msw'
+import Config from 'react-native-config'
 
 import { AuthServices } from '@/api/services'
+import { FieldIsAvailableAPIModel } from '@/domain/Auth'
 import { useDebounce } from '@/hooks/useDebounce/useDebounce'
 import { TestProvider } from '@/providers'
 import { mockFieldIsAvailableApi } from '@/tests/mocks'
-import { AppQueryKeys } from '@/types/api'
+import { serverTest } from '@/tests/server'
+import { AppQueryKeys, END_POINTS_API } from '@/types/api'
 import { HookMocked, ReturnHookMocked } from '@/types/tests'
 
 import { useAuthValueIsAvailable } from './useAuthValueIsAvailable'
@@ -35,7 +39,11 @@ describe('useAuthValueIsAvailable', () => {
 		})
 
 		renderHook(
-			() => useAuthValueIsAvailable('  ', AppQueryKeys.EMAIL_AVAILABLE),
+			() =>
+				useAuthValueIsAvailable({
+					queryKey: AppQueryKeys.EMAIL_AVAILABLE,
+					value: '  ',
+				}),
 			{ wrapper: TestProvider }
 		)
 
@@ -52,7 +60,11 @@ describe('useAuthValueIsAvailable', () => {
 		})
 
 		renderHook(
-			() => useAuthValueIsAvailable('  ', AppQueryKeys.USERNAME_AVAILABLE),
+			() =>
+				useAuthValueIsAvailable({
+					queryKey: AppQueryKeys.USERNAME_AVAILABLE,
+					value: '  ',
+				}),
 			{ wrapper: TestProvider }
 		)
 
@@ -64,7 +76,11 @@ describe('useAuthValueIsAvailable', () => {
 
 	it('should return email available correctly', async () => {
 		const { result } = renderHook(
-			() => useAuthValueIsAvailable('jest', AppQueryKeys.EMAIL_AVAILABLE),
+			() =>
+				useAuthValueIsAvailable({
+					queryKey: AppQueryKeys.EMAIL_AVAILABLE,
+					value: 'jest',
+				}),
 			{ wrapper: TestProvider }
 		)
 
@@ -79,7 +95,11 @@ describe('useAuthValueIsAvailable', () => {
 
 	it('should return username available correctly', async () => {
 		const { result } = renderHook(
-			() => useAuthValueIsAvailable('jest', AppQueryKeys.USERNAME_AVAILABLE),
+			() =>
+				useAuthValueIsAvailable({
+					queryKey: AppQueryKeys.USERNAME_AVAILABLE,
+					value: 'jest',
+				}),
 			{ wrapper: TestProvider }
 		)
 
@@ -89,6 +109,60 @@ describe('useAuthValueIsAvailable', () => {
 			expect(result.current.isAvailable).toBe(
 				mockFieldIsAvailableApi.isAvailable
 			)
+		})
+	})
+
+	it('should disable the query correctly', async () => {
+		renderHook(
+			() =>
+				useAuthValueIsAvailable({
+					queryKey: AppQueryKeys.USERNAME_AVAILABLE,
+					value: 'jest',
+					enabled: false,
+				}),
+			{ wrapper: TestProvider }
+		)
+
+		await waitFor(() => {
+			expect(spyVerifyEmail).not.toHaveBeenCalled()
+			expect(spyVerifyUsername).not.toHaveBeenCalled()
+		})
+	})
+
+	it('should return isUnvailable correctly', async () => {
+		serverTest.use(
+			...[
+				http.get(`${Config.API_URL}${END_POINTS_API.AUTH_VALIDATE_EMAIL}`, () =>
+					HttpResponse.json<FieldIsAvailableAPIModel>(
+						{ isAvailable: false, message: 'false' },
+						{
+							status: 200,
+						}
+					)
+				),
+				http.get(
+					`${Config.API_URL}${END_POINTS_API.AUTH_VALIDATE_USERNAME}`,
+					() =>
+						HttpResponse.json<FieldIsAvailableAPIModel>(
+							{ isAvailable: false, message: 'false' },
+							{ status: 200 }
+						)
+				),
+			]
+		)
+
+		const { result } = renderHook(
+			() =>
+				useAuthValueIsAvailable({
+					queryKey: AppQueryKeys.USERNAME_AVAILABLE,
+					value: 'jest',
+				}),
+			{ wrapper: TestProvider }
+		)
+
+		await waitFor(() => {
+			expect(result.current.isUnvailable).toBe(true)
+			expect(result.current.isAvailable).toBe(false)
 		})
 	})
 })
--- src/domain/Auth/useCases/useAuthValueIsAvailable/useAuthValueIsAvailable.tsx
+++ src/domain/Auth/useCases/useAuthValueIsAvailable/useAuthValueIsAvailable.tsx
@@ -6,11 +6,55 @@ import { AuthApi } from '@/domain/Auth'
 import { useDebounce } from '@/hooks'
 import { AppQueryKeys } from '@/types/api'
 
+type OptionsUseAuthValueIsAvailable = {
+	value: string
+	queryKey: AppQueryKeys.USERNAME_AVAILABLE | AppQueryKeys.EMAIL_AVAILABLE
+	/** @default 1500 */
+	delay?: number
+	/** @default true */
+	enabled?: boolean
+}
+
+/**
+ * A custom hook to check the availability of a value, such as a username or email, with debounced queries.
+ * This hook utilizes `@tanstack/react-query` for efficient data fetching and caching.
+ *
+ * @param {OptionsUseAuthValueIsAvailable} opt - Options for configuring the hook.
+ * @param {string} opt.value - The value to check for availability (e.g., username or email).
+ * @param {AppQueryKeys.USERNAME_AVAILABLE | AppQueryKeys.EMAIL_AVAILABLE} opt.queryKey - The query key to determine whether to check for username or email availability.
+ * @param {number} [opt.delay=1500] - The debounce delay (in milliseconds) before making the query.
+ * @param {boolean} [opt.enabled=true] - Whether the query should be enabled. If `false`, the query will not execute.
+ *
+ * @returns {{
+ *   isAvailable: boolean;
+ *   isUnvailable: boolean;
+ *   isFetching: boolean;
+ * }}
+ *  isAvailable: Indicates if the value is available.
+ *  isUnvailable: Indicates if the value is unavailable.
+ *  isFetching: Indicates if the query is in progress, including during debouncing.
+ * ### Explanation of `isAvailable` and `isUnvailable`
+ * - `isAvailable`: Indicates if the value is available. This is derived from the `data` returned by the query.
+ *   - `true` if `data` is a truthy value (e.g., the API confirmed availability).
+ *   - `false` if `data` is `false` or `undefined` (e.g., the API has not yet confirmed availability).
+ *
+ * - `isUnvailable`: Indicates if the value is unavailable. This specifically checks if `data` is `false`.
+ *   - `true` if the API explicitly returned `false` to indicate unavailability.
+ *   - `false` if `data` is `undefined` (still loading) or `true` (available).
+ *
+ * ### State Behavior Table:
+ * | `data` Value          | `isAvailable` | `isUnvailable` |
+ * |------------------------|---------------|----------------|
+ * | `undefined` (loading) | `false`       | `false`        |
+ * | `true` (available)    | `true`        | `false`        |
+ * | `false` (unavailable) | `false`       | `true`         |
+ *
+ */
 export const useAuthValueIsAvailable = (
-	value: string,
-	queryKey: AppQueryKeys.USERNAME_AVAILABLE | AppQueryKeys.EMAIL_AVAILABLE,
-	delay = 1500
+	opt: OptionsUseAuthValueIsAvailable
 ) => {
+	const { queryKey, value, delay = 1500, enabled = true } = opt
+
 	const { debouncedValue, isDebouncing } = useDebounce(value, delay)
 
 	const queryFn = useMemo(() => {
@@ -26,12 +70,13 @@ export const useAuthValueIsAvailable = (
 		retry: false,
 		staleTime: 20000,
 		gcTime: 1 * 60 * 1000,
-		enabled: !!debouncedValue.trim().length && !isDebouncing,
+		enabled: !!debouncedValue.trim().length && !isDebouncing && enabled,
 		placeholderData: keepPreviousData,
 	})
 
 	return {
 		isAvailable: !!data,
-		isFetching,
+		isUnvailable: data === false,
+		isFetching: isFetching || isDebouncing,
 	} as const
 }
