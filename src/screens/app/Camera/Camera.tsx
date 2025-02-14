c10933fc32344b4babee62c6da69a292b53d317d
Gabriel Bursi <gabriellbursii@gmail.com>
2025-02-13 22:00:35 -0300

    feat(photo): camera component (no tests)

--- src/screens/app/Camera/Camera.tsx
+++ src/screens/app/Camera/Camera.tsx
@@ -1,8 +1,11 @@
 import React, { ComponentProps, useState } from 'react'
 import { Dimensions, StyleSheet } from 'react-native'
 
+import { useIsFocused } from '@react-navigation/native'
+import { Camera, useCameraDevice } from 'react-native-vision-camera'
+
 import { Box, Icon, PermissionManager } from '@/components'
-import { useAppSafeArea } from '@/hooks'
+import { useAppSafeArea, useAppState } from '@/hooks'
 import { CameraScreenProps } from '@/types/screens'
 
 const CAMERA_VIEW = Dimensions.get('screen').width
@@ -12,6 +15,12 @@ const CONTROL_DIFF = 30
 export const CameraScreen = ({ navigation }: Readonly<CameraScreenProps>) => {
 	const { top } = useAppSafeArea()
 	const [flashOn, setFlashOn] = useState(false)
+
+	const device = useCameraDevice('back')
+
+	const isFocused = useIsFocused()
+	const appState = useAppState()
+	const isActive = isFocused && appState === 'active'
 
 	const toggleFlash = () => {
 		setFlashOn((prev) => !prev)
@@ -23,7 +32,13 @@ export const CameraScreen = ({ navigation }: Readonly<CameraScreenProps>) => {
 			description="O aplicativo não tem permissão para acessar a câmera do dispositivo."
 		>
 			<Box flex={1}>
-				<Box backgroundColor="grayWhite" style={StyleSheet.absoluteFill} />
+				{device && (
+					<Camera
+						style={StyleSheet.absoluteFill}
+						device={device}
+						isActive={isActive}
+					/>
+				)}
 				<Box flex={1} justifyContent="space-between">
 					<Box {...$controlAreaTop} style={{ paddingTop: top }}>
 						<Icon
