27729840844efd5ca53703cdae5ed93a81703f39
Gabriel Bursi <gabriellbursii@gmail.com>
2025-03-04 13:57:34 -0300

    chore(onboarding): onboarding images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 export const AppImages = {
 	ImagePlaceholder: require('./image_placeholder.png') as number,
+	OnboardingLight1:
+		require('./images/onboarding/onboarding-light-1.png') as number,
+	OnboardingLight2:
+		require('./images/onboarding/onboarding-light-2.png') as number,
+	OnboardingLight3:
+		require('./images/onboarding/onboarding-light-3.png') as number,
+	OnboardingDark1:
+		require('./images/onboarding/onboarding-dark-1.png') as number,
+	OnboardingDark2:
+		require('./images/onboarding/onboarding-dark-2.png') as number,
+	OnboardingDark3:
+		require('./images/onboarding/onboarding-dark-3.png') as number,
 } as const
